3bda8c20073394fa246fb756a8ae10d80158ab8d
chobpasathai <chobpasathai@gmail.com>
2026-09-09 22:03:34 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('happy-thai-field-v1').then((cache) => {
-      return cache.addAll([
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+const CACHE_NAME = 'happy-thai-field-v2';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondThem ? event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  ) : event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
